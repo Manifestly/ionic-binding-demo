5a4cde2113bc5ac6a535855f6c0a87900f96b1a1
Mark McEahern <mark@mceahern.com>
2016-08-09 14:01:09 -0500

    Simple demo

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -5,7 +5,35 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  private steps: any;
 
+  constructor(public navCtrl: NavController) {
+    this.steps = [
+      {
+        title: 'a',
+        completed_at: null,
+        header_step: true
+      },
+      {
+        title: 'a 1',
+        completed_at: null,
+        header_step: false
+      },
+      {
+        title: 'a 2',
+        completed_at: Date.now(),
+        header_step: false
+      }
+    ];
+  }
+
+  public toggleStep(step: any): void {
+    console.log(step);
+    if (step.completed_at) {
+      step.completed_at = null;
+    } else {
+      step.completed_at = Date.now();
+    }
+    console.log(step);
   }
 }
